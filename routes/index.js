9a9fc74b118e91a8657937fd70a99ebfe1964a39
Sebastian Salamanca <sebastiansalamanca0@gmail.com>
2018-01-19 11:52:24 -0500

    cache module added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var apicache  = require('apicache')
+var cache     = apicache.middleware
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', cache('1 minute'), function(req, res, next) {
 	// use  " req.query.<youVarName>' " to get url params
 	return res.status(200).send("api working")
 });
